4a348cf9aef29f003d0b96c3d4c0f89f32485aaf
Calin101 <camillealin13200@gmail.com>
2020-04-15 12:28:14 +0200

    site CV

--- scripts/graph-quality.js
+++ scripts/graph-quality.js
@@ -9,7 +9,7 @@ function drawChart() {
     ['Curiosité', 80, 'stroke-color: #703593; stroke-width: 2; fill-color: rgb(81, 159, 182)'],
     ['Autonomie', 90, 'stroke-color: #703593; stroke-width: 2; fill-color: rgb(81, 159, 182)'],
     ['Adaptabilité', 95, 'stroke-color: #703593; stroke-width: 2; fill-color: rgb(24, 106, 130)'],
-    ['Rigeur', 90, 'stroke-color: #703593; stroke-width: 2; fill-color: rgb(81, 159, 182)'],
+    ['Rigueur', 90, 'stroke-color: #703593; stroke-width: 2; fill-color: rgb(81, 159, 182)'],
 ]);
 
 var options = { title: "Qualités",
